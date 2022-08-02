62fc2c4f71ad44612ac4bdec5c33e5c5cfd76e31
Thiago Silva <thiago380tt@gmail.com>
2022-08-02 01:15:06 -0300

    criação do component SubscribeButton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 import styles from './home.module.scss';
 
@@ -16,7 +17,10 @@ export default function Home() {
           Get access to all the publications <br />
           <span>for $9.90 month</span>
           </p>
+          <SubscribeButton />
         </section>
+
+       
 
         <img src="./imagens/avatar.svg" alt="Girl coding" />
       </main>
