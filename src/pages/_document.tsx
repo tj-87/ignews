2ee05b084a49f0fe72293895ee5fa3a17130c486
Thiago Silva <thiago380tt@gmail.com>
2022-08-02 00:01:26 -0300

    Componentes Header e SignInButton

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
@@ -7,6 +7,7 @@ export default class MyDocument extends Document {
         <Head> 
             <link rel="preconnect" href="https://fonts.gstatic.com" />
             <link href="https://fonts.googleapis.com/css2?family=Poppins:ital@1&family=Roboto+Mono&family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet"/> 
+            <link rel="shortcut icon" href="./imagens/favicon.png" type="image/png"/>
         </Head>
         <body>
           <Main />
